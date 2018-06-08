004281e156a723434212b451d463083f55a5219f
Daya <dw129@enovate-it.com>
2018-06-08 19:04:29 +0530

    musicplayer ui

--- App.js
+++ App.js
@@ -5,7 +5,7 @@
  */
 
 import React, { Component } from 'react';
-import { createBottomTabNavigator, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator, createSwitchNavigator, createStackNavigator } from 'react-navigation';
 import Screen1 from './Screens/Screen1';
 import Screen2 from './Screens/Screen2';
 import Screen3 from './Screens/Screen3';
@@ -13,14 +13,32 @@ import Screen4 from './Screens/Screen4';
 import Splash from './Screens/Splash';
 
 import { Icon } from 'react-native-elements'
+import { TouchableHighlight,Image, BackHandler } from 'react-native'
 
 
 
+const playerScreenStack = createStackNavigator({
+  PlayerScreen: {
+    screen: Screen1, navigationOptions: ({ navigation, goBack }) =>
+      ({
+        title: 'Podsource',
+        headerLeft: <TouchableHighlight style={{ marginLeft: 20}}  onPress={() => BackHandler.exitApp()}>
+          <Image style={{width: 20, height: 20 }} source={require('./assets/images/cancel.png')} />
+        </TouchableHighlight>,
+        headerStyle: {
+
+          backgroundColor: '#3D4246'
+
+        },
+        headerTintColor: '#fff',
+      }),
+  }
+});
 
 
 const App = createBottomTabNavigator({
   Screen1: {
-    screen: Screen1, navigationOptions: {
+    screen: playerScreenStack, navigationOptions: {
       title: 'Options',
       tabBarIcon: () => <Icon
         name='more-horiz' />
@@ -30,17 +48,23 @@ const App = createBottomTabNavigator({
     screen: Screen2, navigationOptions: {
       title: 'Collection',
       tabBarIcon: () => <Icon
-      name='collections' />
+        name='collections' />
     }
   },
   Screen3: {
     screen: Screen3, navigationOptions: {
       title: 'Bookmark',
       tabBarIcon: () => <Icon
-      name='bookmark' />
+        name='bookmark' />
     }
   }
 }, {
+    tabBarOptions: {
+      activeTintColor: 'white',
+      style: {
+        backgroundColor: '#3D4246'
+      }
+    },
     headerMode: 'screen',
     backBehavior: 'none',
     initialRouteName: 'Screen1'
