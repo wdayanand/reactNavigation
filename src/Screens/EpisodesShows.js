0c8479ff56b153f49e2e1cc030652b8b718e5bc9
Shilpa <sg192@enovate-it.com>
2018-07-20 12:32:33 +0530

    add new file with segment controll

---
+++ src/Screens/EpisodesShows.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { SearchBar } from 'react-native-elements'
+import {View} from 'react-native'
+
+
+
+export default class EpisodesShows extends React.Component {
+    render() {
+
+        return (
+            <View style={{ flex: 1, paddingTop: 20 }}>
+                <SearchBar
+                    onChangeText={someMethod}
+                    onClear={someMethod}
+                    placeholder='Type Here...' />
+            </View>
+        );
+    }
+}
